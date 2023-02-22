1580d60d878def1fbc8fd83b4e6df2ea37523f8d
Ishita <ishitagops12@gmail.com>
2023-02-22 14:04:51 +0530

    lifecycle component prog done

--- 12. Feb22/src/Lifecycle.js
+++ 12. Feb22/src/Lifecycle.js
@@ -5,10 +5,18 @@ class Lifecycle extends React.Component{
         constructor(props){
                 super(props);
                 this.state={
-                        number:4
+                        number:5
                 };
         }
+
+        // forceUpdate(){
+
+        //     console.log("forceUpdate");
+        //     this.setState({nuumber:50});
+        // }
+
         componentDidMount(){
+
                 console.log("Lifecycle component is mounted " + this.state.number);
                 console.log(this.props.a);
         }
@@ -19,9 +27,45 @@ class Lifecycle extends React.Component{
 
         }
 
+        getSnapshotBeforeUpdate(prevProps, prevState){
+
+            console.log("PrevState "+prevState.number);
+            console.log("PrevProps "+prevProps.a);
+
+        }
+
+        componentDidUpdate(){
+
+            console.log("Current State "+this.state.number);
+
+        }
+
+        shouldComponentUpdate(){
+
+            // return false;
+            return true;
+        }
+
+        componentWillUnmount(){
+
+            console.log("Images unmounted");
+        }
+
+        changeNum=()=>{
+            
+            this.setState({number:4});
+        }
+
+
         render(){
+
+                // this.forceUpdate();
                 return(
-                        <h1>Hi</h1>
+                        <div>
+                        <h1>LifeCycle Component Phases</h1>
+                        <button onClick={this.changeNum}>Click here</button>
+                        <h1>{this.state.number}</h1>
+                        </div>
                 );
         }
 }
