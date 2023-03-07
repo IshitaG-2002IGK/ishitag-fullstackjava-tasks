6b06d656ba0397b51d8d1241750b77828d1b7c0a
Ishita <ishitagops12@gmail.com>
2023-03-07 19:22:50 +0530

    Deleted file to be updated

--- 13. Mar7-newapp/src/App.js
+++ 13. Mar7-newapp/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+    <Routes>
+        <Route  path='/' element={<Home></Home>}></Route>;
+        <Route  path='/AboutUs' element={<AboutUs></AboutUs>}></Route>;
+        <Route  path='/ContactUs' element={<ContactUs></ContactUs>}></Route>;
+    </Routes>
+
     </div>
   );
 }
---
+++ 13. Mar7-newapp/src/ContactUs.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+
+export default function ContactUs() {
+  return (
+    <div>
+        <h1>
+            Contact Us !
+        </h1>
+    </div>
+  )
+}
