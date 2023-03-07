fb0596ea00434597faf8e1737a4e6e81d7db3696
Ishita <ishitagops12@gmail.com>
2023-03-07 11:29:43 +0530

    code cleanup

--- 13. Mar7-newapp/src/index.js
+++ 13. Mar7-newapp/src/index.js
@@ -16,12 +16,10 @@ root.render(
     <HelloClass /><br></br>
     <HelloClassSimple /><br></br>
     <HelloCounter /><br></br>
-    {/* <Toggle /> */}
+    <Toggle /><br></br><br></br>
     <ToggleNew /><br></br>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
