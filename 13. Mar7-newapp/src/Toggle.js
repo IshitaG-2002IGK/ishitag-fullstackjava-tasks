fd93767e2f7039166c378356436601ac5f762343
Ishita <ishitagops12@gmail.com>
2023-03-07 10:48:40 +0530

    toggles modified

--- 13. Mar7-newapp/src/Toggle.js
+++ 13. Mar7-newapp/src/Toggle.js
@@ -5,26 +5,33 @@ const Toggle = () => {
   //Using Inline Function and the The Logical Not (!) to toggle state
   const [toggle, setToggle] = useState(true)
 
+  function popUp() {
+    setToggle(!toggle);
+    alert("Activation");
+
+  }
+
   return (
     <>
     <br></br>
       <button 
-            onClick={() => setToggle(!toggle)}> Toggle Button
+            onClick={popUp}>Toggle Button
+            
+            {/* onClick=() => {setToggle(!toggle)} */}
       </button>
       
       {/* Method 1: */}
       
-      {toggle && 
+      {/* {toggle && 
       (
         <p><b>Toggle button activated!</b></p>
-      )}
+      )} */}
 
       {/* Method 2: */}
 
-      {/* {
+      {
         toggle? <p><b>Toggle button activated!</b></p>: null
-      } */
-      }
+      } 
 
     </>
   )
